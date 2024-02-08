a5d1d38d451b17cd0bf225c84fff03c3c95fbc94
Hamza-Asif-ha <hasif6034@gmail.com>
2024-02-08 21:39:33 +0500

    Initial commit

---
+++ lib/pages/Wheel.dart
@@ -0,0 +1,409 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+import 'package:rxdart/rxdart.dart';
+
+class Wheel extends StatefulWidget {
+  Wheel({super.key});
+
+  @override
+  State<Wheel> createState() => _WheelState();
+}
+
+final selected = BehaviorSubject<int>();
+
+class _WheelState extends State<Wheel> {
+  int rewards = 0;
+  int balance = 0;
+  int tries = 5;
+  bool animate = false;
+  int points = 5;
+  List<int> numbers = [
+    50,
+    100,
+    500,
+    1000,
+    2000,
+    5000,
+    10000,
+    50000,
+  ];
+
+  List<int> point = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+  ];
+
+  void resetAll(bool reset) {
+    if (reset) {
+      tries = 5;
+      rewards = 0;
+      balance = 0;
+      points = 0;
+      animate = false;
+    }
+  }
+
+  @override
+  void dispose() {
+    //selected.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          18,
+                        ),
+                        color: Colors.grey[300],
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(4, 4),
+                            color: Colors.grey.shade500,
+                            blurRadius: 8,
+                            spreadRadius: 2,
+                          ),
+                          BoxShadow(
+                            offset: Offset(-4, -4),
+                            color: Colors.white,
+                            blurRadius: 12,
+                            spreadRadius: 0,
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        'Points: ${points}',
+                        style: GoogleFonts.bebasNeue(
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Text(
+                  'Time to Roll the Dice and See What Luck Has in Store for You!',
+                  style: GoogleFonts.bebasNeue(
+                    fontSize: 24,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(4, 4),
+                      color: Colors.grey.shade500,
+                      blurRadius: 15,
+                      spreadRadius: 1,
+                    ),
+                    BoxShadow(
+                      offset: Offset(-4, -4),
+                      color: Colors.white,
+                      blurRadius: 15,
+                      spreadRadius: 1,
+                    ),
+                  ],
+                  shape: BoxShape.circle,
+                ),
+                height: 300,
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    FortuneWheel(
+                      styleStrategy: UniformStyleStrategy(),
+                      animateFirst: animate,
+                      selected: selected.stream,
+                      items: [
+                        for (int i = 0;
+                            i < numbers.length;
+                            i++) ...<FortuneItem>{
+                          FortuneItem(
+                            style: FortuneItemStyle(
+                              color: i % 2 == 0
+                                  ? Colors.white
+                                  : Colors.grey.shade300,
+                              borderColor: Colors.white,
+                              borderWidth: 0,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Text(
+                                  '${numbers[i]}',
+                                  style: GoogleFonts.bebasNeue(
+                                    fontSize: 22,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 16.0),
+                                  child: LottieBuilder.network(
+                                    "https://lottie.host/764a8e34-a26e-4967-a115-73530693de3a/XtcKcLCLgO.json",
+                                    height: 40,
+                                    width: 40,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        },
+                      ],
+                      onAnimationEnd: () {
+                        setState(
+                          () {
+                            rewards = numbers[selected.value];
+                            balance = balance + numbers[selected.value];
+                            points = points + point[selected.value];
+                            tries--;
+                            animate = false;
+                          },
+                        );
+
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text(
+                                'Congratulations!',
+                                style: GoogleFonts.bebasNeue(
+                                  fontSize: 30,
+                                ),
+                              ),
+                              content: Text(
+                                'You just won $rewards points.',
+                                style: GoogleFonts.bebasNeue(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    // LottieBuilder.network(
+                    //   'https://lottie.host/c426982c-8305-4cf9-a9da-9113737f353c/UCgKUFv1MA.json',
+                    //   animate: animate,
+                    // ),
+                    Positioned(
+                      bottom: 119,
+                      child: GestureDetector(
+                        onTap: () {
+                          if (tries > 0) {
+                            animate = true;
+                            setState(() {
+                              selected.add(
+                                Fortune.randomInt(0, numbers.length),
+                              );
+                            });
+                          } else {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Container(
+                                  height: 150,
+                                  width: 150,
+                                  child: AlertDialog(
+                                    backgroundColor: Colors.grey[300],
+                                    title: Text(
+                                      'Ooopss.... ;(',
+                                      style: GoogleFonts.bebasNeue(
+                                        fontSize: 30,
+                                      ),
+                                    ),
+                                    content: Text(
+                                      'You have Zero Tries :(',
+                                      style: GoogleFonts.bebasNeue(
+                                        color: Colors.grey,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          }
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(4, 4),
+                                color: Colors.grey.shade500,
+                                blurRadius: 15,
+                                spreadRadius: 1,
+                              ),
+                              BoxShadow(
+                                offset: Offset(-4, -4),
+                                color: Colors.white,
+                                blurRadius: 15,
+                                spreadRadius: 1,
+                              ),
+                            ],
+                            color: Colors.grey.shade300,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12.0, horizontal: 12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: Container(
+                            padding: EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(
+                                18,
+                              ),
+                              color: Colors.grey[300],
+                              boxShadow: [
+                                BoxShadow(
+                                  offset: Offset(4, 4),
+                                  color: Colors.grey.shade500,
+                                  blurRadius: 8,
+                                  spreadRadius: 2,
+                                ),
+                                BoxShadow(
+                                  offset: Offset(-4, -4),
+                                  color: Colors.white,
+                                  blurRadius: 12,
+                                  spreadRadius: 0,
+                                ),
+                              ],
+                            ),
+                            child: Text(
+                              'Balance: ${balance}',
+                              style: GoogleFonts.bebasNeue(
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 14.0),
+                          child: Container(
+                            padding: EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(
+                                18,
+                              ),
+                              color: Colors.grey[300],
+                              boxShadow: [
+                                BoxShadow(
+                                  offset: Offset(4, 4),
+                                  color: Colors.grey.shade500,
+                                  blurRadius: 8,
+                                  spreadRadius: 2,
+                                ),
+                                BoxShadow(
+                                  offset: Offset(-4, -4),
+                                  color: Colors.white,
+                                  blurRadius: 12,
+                                  spreadRadius: 0,
+                                ),
+                              ],
+                            ),
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 12.0),
+                              child: Text(
+                                'Tries: ${tries}',
+                                style: GoogleFonts.bebasNeue(
+                                  fontSize: 24,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          resetAll(true);
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              offset: Offset(4, 4),
+                              color: Colors.grey.shade500,
+                              blurRadius: 8,
+                              spreadRadius: 2,
+                            ),
+                            BoxShadow(
+                              offset: Offset(-4, -4),
+                              color: Colors.white,
+                              blurRadius: 12,
+                              spreadRadius: 0,
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(40.0),
+                          child: Text(
+                            'Reset All',
+                            style: GoogleFonts.bebasNeue(
+                              fontSize: 34,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/welcome.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+
+import 'Wheel.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Text
+              Text(
+                'Luckki Wheel',
+                style: GoogleFonts.pacifico(
+                  fontSize: 54,
+                ),
+              ),
+              //animation
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: LottieBuilder.network(
+                    "https://lottie.host/769cc02a-a150-4cd9-a724-1a83d7df9a4b/XHdqQUtM2L.json"),
+              ),
+
+              //button
+
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Wheel(),
+                    ),
+                  );
+                },
+                child: Container(
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(4, 4),
+                          color: Colors.grey.shade500,
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                        ),
+                        BoxShadow(
+                          offset: Offset(-4, -4),
+                          color: Colors.white,
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                        ),
+                      ]),
+                  child: Text(
+                    'Get Started',
+                    style: GoogleFonts.bebasNeue(
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
